04014ac3511665825bae8f02e12e3189b868d7bf
Scott Michaud <scott@scott-michaud.com>
2021-10-24 21:44:18 -0400

    Initial Commit

---
+++ TestAwaitCycle/AwaitableTaskContainer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestAwaitCycle
+{
+    /// <summary>
+    /// Simple example of using TaskCompletionSource to create an awaitable
+    /// consumer/producer FIFO container.
+    /// </summary>
+    /// <typeparam name="T">The item to store in the container</typeparam>
+    public class AwaitableTaskContainer<T>
+    {
+        // Mutex for Read/Write access.
+        private readonly object _lock = new();
+        // Stored values waiting to be consumed.
+        private readonly Queue<T> _storedValues = new();
+        // A promise left by a consumer.
+        private TaskCompletionSource<T>? _waitingTask;
+
+        /// <summary>
+        /// The consumer calls this to acquire a Task (promise) for the next value.
+        /// </summary>
+        /// <returns>A Task that will be fulfilled when a value is available.</returns>
+        public Task<T> GetValue()
+        {
+            TaskCompletionSource<T> newResult = CreateTaskSource();
+            
+            lock (_lock)
+            {
+                if (_storedValues.Count > 0)
+                {
+                    // If a producer has left at least one item for us, grab it without waiting.
+                    newResult.SetResult(_storedValues.Dequeue());
+                }
+                else
+                {
+                    // Otherwise, leave a promise for a producer to fulfill.
+                    _waitingTask = newResult;
+                }
+            }
+
+            // Return the Task handle from the promise (which may or may not be fulfilled).
+            return newResult.Task;
+        }
+
+        /// <summary>
+        /// Producer(s) call this to add a value to the queue. This will wake up the consumer
+        /// if it is waiting on a value.
+        /// </summary>
+        /// <param name="value">The value to add to the queue.</param>
+        public void AddValue(T value)
+        {
+            lock (_lock)
+            {
+                // Add the value for the consumer at the tail of the queue.
+                _storedValues.Enqueue(item: value);
+
+                // If there is a consumer waiting, give them the item at the head.
+                if (_waitingTask is null == false)
+                {
+                    _waitingTask.SetResult(_storedValues.Dequeue());
+                }
+            }
+        }
+        
+        // Always run asynchronously.
+        private static TaskCompletionSource<T> CreateTaskSource()
+        {
+            return new TaskCompletionSource<T>(creationOptions: TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+    }
+}
---
+++ TestAwaitCycle/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestAwaitCycle
+{
+    public static class Program
+    {
+        public static async Task<int> Main()
+        {
+            AwaitableTaskContainer<int> tasks = new();
+            
+            // Start Producer but do not wait on "producer" Task (outside of Task.Run()).
+            var producer = Task.Run(async () => { await Producer(argTasks: tasks); });
+
+            // Wait six seconds for elements to queue up.
+            await Task.Delay(6000);
+            
+            // Start Consumer but do not wait on "consumer" Task (outside of Task.Run()).
+            var consumer = Task.Run(async () => { await Consumer(argTasks: tasks); });
+
+            // Wait until both producer and consumer Task.Run() Tasks finish (which is never).
+            await Task.WhenAll(producer, consumer);
+            
+            return 0;
+        }
+
+        private static async Task Producer(AwaitableTaskContainer<int> argTasks)
+        {
+            // Just a unique value per item.
+            var idx = 0;
+            // Demo-code has no end condition.
+            while (true)
+            {
+                // Rate limit to one per second.
+                await Task.Delay(1000);
+                    
+                Console.WriteLine($"Creating item {++idx}");
+                argTasks.AddValue(value: idx);
+            }
+            // ReSharper disable once FunctionNeverReturns Reason: Infinite demo.
+        }
+
+        private static async Task Consumer(AwaitableTaskContainer<int> argTasks)
+        {
+            // Demo-code has no end condition.
+            while (true)
+            {
+                Console.WriteLine($"Grabbing item {await argTasks.GetValue()}");
+            }
+            // ReSharper disable once FunctionNeverReturns Reason: Infinite demo.
+        }
+    }
+}
